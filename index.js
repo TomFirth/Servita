9553c4c6e102ca42e33f4efb46efc952385b5ae6
TomFirth <tom.firth84@gmail.com>
2024-04-16 15:37:15 +0100

    tidy up

--- index.js
+++ index.js
@@ -9,22 +9,22 @@ let allValues
 
 function iterate (object) {
     Object.keys(object).forEach(key => {
-        // console.log("key:", key, "value:", object[key])
         if (key == "start") startArray.push(object[key])
         if (key == "valueCoding") valueCodings.push({
            [object[key].code]: object[key].display
         })
+        // recursive search
         if (typeof object[key] === 'object' && object[key] !== null) {
             iterate(object[key])
         }
     })
-    return { startArray:startArray.sort(), valueCodings }
+    return { startArray: startArray.sort(), valueCodings }
 }
 
 app.get('/aggregate', (req, res) => {
     axios.get("https://directory.spineservices.nhs.uk/STU3/Organization/N81082")
     .then(response => {
-        valueCodings = []
+        valueCodings = [] // clean up array
         allValues = iterate(response.data.extension)
         res.json({
             maxStart: allValues.startArray[startArray.length-1],
