51a615bdb36e96491df26e2e21a90310218d97e5
TomFirth <tom.firth84@gmail.com>
2024-04-18 14:59:52 +0100

    updates

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
-const express = require('express')
-const app = express()
-const port = 8080
-const axios = require("axios")
+import express from 'express';
+import axios from 'axios';
 
-let startArray = []
-let valueCodings = []
-let allValues
+const app = express();
 
-function iterate (object) {
-    Object.keys(object).forEach(key => {
-        if (key == "start") startArray.push(object[key])
-        if (key == "valueCoding") valueCodings.push({
-           [object[key].code]: object[key].display
-        })
-        // recursive search
-        if (typeof object[key] === 'object' && object[key] !== null) {
-            iterate(object[key])
-        }
-    })
-    return { startArray: startArray.sort(), valueCodings }
-}
+let startArray = [], valueCodings = [];
+
+const iterate = (object) => {
+  Object.keys(object).forEach(key => {
+    if (key === "start") startArray.push(object[key]);
+    if (key === "valueCoding") {
+      valueCodings.push({
+        [object[key].code]: object[key].display
+      });
+    }
+    if (typeof object[key] === 'object' && object[key] !== null) {
+      iterate(object[key]);
+    }
+  });
+  return { startArray: startArray.sort(), valueCodings };
+};
 
 app.get('/aggregate', (req, res) => {
-    axios.get("https://directory.spineservices.nhs.uk/STU3/Organization/N81082")
+  axios.get('https://directory.spineservices.nhs.uk/STU3/Organization/N81082')
     .then(response => {
-        valueCodings = [] // clean up array
-        allValues = iterate(response.data.extension)
-        res.json({
-            maxStart: allValues.startArray[startArray.length-1],
-            minStart: allValues.startArray[0],
-            valueCodings: allValues.valueCodings
-        })
+      const { startArray, valueCodings } = iterate(response.data.extension);
+      res.status(200).json({
+        maxStart: startArray[startArray.length - 1],
+        minStart: startArray[0],
+        valueCodings
+      });
     })
-})
+    .catch(error => {
+      console.error(error);
+      res.status(500).send(error);
+    });
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(8080, () => {
+  console.log('Ready');
+});
